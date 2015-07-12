37e9eb11cab1bac71c48188350b50b3b9bfcd6d9
michaelkunc <mjkunc@gmail.com>
2015-07-12 13:18:28 -0500

    form is complete, posting to the db and redirect is working

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,37 @@ router.get("/posts", function(req, res) {
     });
 });
 
+// GET NEW post
+router.get("/posts/new", function(req, res) {
+    res.render('newpost', {
+        title: "Write an excellent new post"
+    });
+});
+
+// POST NEW post
+router.post('/posts', function(req, res) {
+    // db variables
+    var db = req.db;
+    var collection = db.get('posts');
+
+    // form variables
+    var title = req.body.title;
+    var body = req.body.body;
+    var author = req.body.author;
+    var date = req.body.date;
+
+    collection.insert({
+            "title": title,
+            "body": body,
+            "author": author,
+            "date": date
+        }, function(err, doc) {
+            if (err) {
+                res.send("There was a problem updating the DB");
+            } else {
+              res.redirect('posts');
+        }
+    });
+});
 
 module.exports = router;
